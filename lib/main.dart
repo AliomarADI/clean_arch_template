d28a8b98816ea93b95b99a016e6628f0a6eee74e
AliomarADI <aliomaramerica@gmail.com>
2022-12-15 21:57:58 +0600

    main cofigure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:clean_arch_template/core/injection/configurator.dart';
 import 'package:clean_arch_template/feature/app/app.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // localization entry point
+  await EasyLocalization.ensureInitialized();
+
+  // dependency injection entry point
+  configureDependencies();
+
+  // set landscape orientation
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(
     EasyLocalization(
       supportedLocales: const [
